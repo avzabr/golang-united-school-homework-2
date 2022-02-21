03e21757e502f0eeb708395aa4720eafa1869e59
Aliaksandr Zabrodski <aliaksandr.zabrodski@ask.com>
2022-02-21 09:58:03 +0300

    Homework 2

--- solution.go
+++ solution.go
@@ -1,5 +1,17 @@
 package square
 
+import "math"
+
+type Shape int
+
+const (
+	SidesCircle Shape = iota
+	_
+	_
+	SidesTriangle
+	SidesSquare
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +20,14 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum Shape) float64 {
+	switch sidesNum {
+	case SidesSquare:
+		return math.Pow(sideLen, 2)
+	case SidesCircle:
+		return math.Pi * math.Pow(sideLen, 2)
+	case SidesTriangle:
+		return math.Sqrt(math.Pow(sideLen, 2) * math.Sqrt(3) / 4)
+	}
+	return 0
 }
